7ed0fe64af917e2bfc3f835c7edd474955f6a2fc
Elena Sokolovskaya <ev.sokolovskaya@gmail.com>
2024-03-07 20:46:07 -0800

    answers for lesson 1

--- 01-node-tutorial/answers/03-module.js
+++ 01-node-tutorial/answers/03-module.js
@@ -2,6 +2,7 @@
 const persons = require ('./04-names')
 const sayHi = require('./05-utils');
 const data = require('./06-alternative-flavor');
+require('./07-mind-grenade')
 
 // Print the names of each person
 // console.log('Person 1:', person1);
