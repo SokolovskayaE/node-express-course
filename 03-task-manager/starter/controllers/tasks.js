fcc9cb07459bf446a31c08c1f4f66c7b7145f336
Elena Sokolovskaya <ev.sokolovskaya@gmail.com>
2024-04-30 00:03:40 -0700

    answers for lesson 6

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -23,7 +23,7 @@ const createTask = asyncWrapper(async (req, res) => {
    res.status(201).json({task}) 
 })
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
    const {id:taskID} = req.params;
    const task = await Task.findOneAndDelete({_id:taskID});
    if(!task){
@@ -32,7 +32,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
    res.status(200).json({task})
 })
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
 const {id:taskID} = req.params;
 const task = await Task.findOneAndUpdate({_id:taskID},req.body, {
    new:true,
